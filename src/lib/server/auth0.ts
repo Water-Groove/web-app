02905f86a02f41349ca410218a437a9c1232f9ff
ultimatefaloe <ultimatefaloe@outlool.com>
2026-01-04 15:53:37 +0100

    enviroment variable added

--- src/lib/server/auth0.ts
+++ src/lib/server/auth0.ts
@@ -1,8 +1,10 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
+const audiwnceUrl = process.env.AUTH0_AUDIENCE;
+
 export const auth0 = new Auth0Client({
   authorizationParameters: {
-    audience: "https://api.watergroove.com",
+    audience: audiwnceUrl,
     scope: "openid profile email",
   },
   session: {
